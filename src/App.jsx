e08ac37865c5042273abd15b937644ce1481973e
Viyom Verma <viyomverma3107@gmail.com>
2025-07-23 21:01:39 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from "react"
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
-import Navbar from './components/Navbar'; 
-import Home from './pages/Home';
-import Quiz from './pages/Quiz';
-import Counselling from './pages/Counselling';
-import About from './pages/About';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Counselling from "./pages/Counselling";
+import About from "./pages/About";
 import Jobs from "./pages/Jobs";
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Navigation bar on all pages */}
-       <Routes>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/counselling" element={<Counselling />} />
         <Route path="/about" element={<About />} />
-        <Route path="/jobs" element={<Jobs/>}/>
+        <Route path="/jobs" element={<Jobs />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'career-compass' with your actual GitHub repo name
 export default defineConfig({
+  base: '/career-compass/',
   plugins: [react()],
 })
