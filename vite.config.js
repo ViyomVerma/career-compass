0b250f4baadd7f5623e630f6cf476ef288b2c37f
Viyom Verma <viyomverma3107@gmail.com>
2025-07-24 12:56:31 +0530

    Deploy Career Compass

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'career-compass' with your actual GitHub repo name
-// vite.config.js
 export default defineConfig({
+  base: '/career-compass/', // 👈 this must match your deployment subpath
   plugins: [react()],
-  base: "/career-compass/", // <--- set to your repo name
-});
+})
