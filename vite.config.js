2f3a49f99e2f1b862a4aba60a0f6bcd11f5dae7f
Viyom Verma <viyomverma3107@gmail.com>
2025-07-25 15:12:40 +0530

    fix: correct Vite base path for Render

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/career-compass/', // this matches your repo/folder name
+  base: './', // this matches your repo/folder name
   plugins: [react()]
 })
